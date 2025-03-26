d26db08ea0bcd181249814f126913896ddd1173c
Vo Hoai Nam <south@nexondv.com>
2025-03-26 14:52:55 +0700

    feat: update config

--- lua/plugins/auto_restore_sessions.lua
+++ lua/plugins/auto_restore_sessions.lua
@@ -1,6 +1,7 @@
+if not vim.g.vscode then return {} end
 return {
   "AstroNvim/astrocore",
-  ---@type AstroCoreOpts 
+  ---@type AstroCoreOpts
   opts = {
     autocmds = {
       -- disable alpha autostart
