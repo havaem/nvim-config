4651d269d93c35693387fe2e1c0447caf30a1169
Vo Hoai Nam <south@nexondv.com>
2025-06-23 21:48:42 +0700

    chore: update description of an util function

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,7 +1,7 @@
 --- Merges two tables into a new table.
 ---@param t1 vim.keymap.set.Opts: The first table
 ---@param t2 vim.keymap.set.Opts: The second table
--- @return table: A new table containing all keys and values from t1 and t2
+---@return table: A new table containing all keys and values from t1 and t2
 local function merge(t1, t2)
   local result = {}
   for k, v in pairs(t1) do
